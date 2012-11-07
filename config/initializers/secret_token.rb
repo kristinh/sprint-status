159ac32f2086901f8832575adffc17ed6d5c1181
kristinh <kristin@precis-project.com>
2012-11-07 06:00:12 -0800

    Initial commit of project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SprintStats::Application.config.secret_token = '3fd08174ab58758f02197a18d051567be43dd78c5a34dbb03497f739a2fc9416e29462549cf9409621e22b9c5c1096fbc5a701edc862d8663768c18b7b33ef3f'
