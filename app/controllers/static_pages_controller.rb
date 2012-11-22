e596eb0c3b1d2d68adf736318a9ad370a4d576ce
kristinh <kristin@precis-project.com>
2012-11-22 06:00:54 -0800

    Finishes layout skeleton and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    # get actual sprint
+    # get planned points for each team for actual sprint
+    # get last 5 & last 3 stats for each team
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SprintStats::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  root :to => 'static_pages#home'
 
-  get "static_pages/about"
+  #get "static_pages/help"
+  match '/help', to: 'static_pages#help'
+
+  #get "static_pages/about"
+  match '/about', to: 'static_pages#about'
 
   resources :sprint_results
 
