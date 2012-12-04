d7112e3f3a51c5a96a9cbfbb39b3738ad287d370
kristinh <kristin@precis-project.com>
2012-12-04 05:53:25 -0800

    Shows teams and adds actions

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
 
-    today = Date.today
+    @today = Date.today
 
     @current_sprint_results = 
       SprintResult
         .joins(:sprint)
-        .where(["sprints.end >= ? AND sprints.start <= ?", today, today])
+        .where(["sprints.end >= ? AND sprints.start <= ?", @today, @today])
         .order("sprints.id ASC, sprints.start ASC")
         .includes(:sprint, :team)
+
+    @teams = Team.includes(:team_stats)
+
 
     respond_to do |format|
       format.html 
